890c55938474b7e237e41cc759312f0605355157
hardyreinhold <hardykaasik@gmail.com>
2025-04-05 10:38:34 +0300

    renaming branch

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int MoveSpeed;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move()
+    }
+
+    void Move()
+    {
+        transform
+    }
+}
